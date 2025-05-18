7217cc403fc5f09e1ccc4452d4f3fc01aa383043
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2025-05-18 21:55:55 +0530

    Setup react testing

---
+++ tests/App.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import App from "../src/App.tsx";
+
+describe('App', () => {
+  it('should render App', () => {
+    render(<App />)
+    expect(screen.getByText("Vite + React")).toBeInTheDocument()
+  })
+})
