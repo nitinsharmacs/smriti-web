1bbe36a241b0d26c42265535ee3782e608fb2736
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2025-05-21 12:37:40 +0530

    [#1] Add shorthands for imports

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import App from "../src/App.tsx";
+import App from "src/App";
 
 describe('App', () => {
   it('should render App', () => {
