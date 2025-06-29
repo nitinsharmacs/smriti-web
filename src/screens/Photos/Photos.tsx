78f8a18073cec93cdb5465c5622e5793320c5033
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2025-06-29 18:37:16 +0530

    Integrate minimal image grid

--- src/screens/Photos/Photos.tsx
+++ src/screens/Photos/Photos.tsx
@@ -1,5 +1,31 @@
+import './photos.css';
+
+import { Grid } from 'scrubbable-photo-grid';
+import type {
+  GridConfigType,
+  SectionType,
+} from 'scrubbable-photo-grid/dist/types';
+
+import { createRef } from 'react';
+import images from '../../../data/images.json';
+
+const gridData = images.slice(0, 10) as SectionType[];
+
+const gridConfig: GridConfigType = {
+  containerWidth: 900,
+  targetRowHeight: 150,
+  segmentMargin: 5,
+  sectionMargin: 10,
+};
+
 const Photos = () => {
-  return <div>Phoytos</div>;
+  const ref = createRef<HTMLDivElement>();
+
+  return (
+    <div className='photos' ref={ref}>
+      <Grid gridData={gridData} config={gridConfig} parent={ref} />
+    </div>
+  );
 };
 
 export default Photos;
