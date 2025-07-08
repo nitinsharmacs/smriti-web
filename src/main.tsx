9a15c9d596590981ab618787fa48011df7484dbe
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2025-07-08 20:57:29 +0530

    Adjust styling for photo grid

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "./fonts/opun-mai/OpunMaiRegular.ttf"
-import "./fonts/opun-mai/OpunMaiLight.ttf"
-import "./fonts/opun-mai/OpunMaiBold.ttf"
-import './index.css'
-import App from './App.tsx'
-import { StyledEngineProvider } from '@mui/material'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './fonts/opun-mai/OpunMaiRegular.ttf';
+import './fonts/opun-mai/OpunMaiLight.ttf';
+import './fonts/opun-mai/OpunMaiBold.ttf';
+import './index.css';
+import App from './App.tsx';
+import { StyledEngineProvider } from '@mui/material';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StyledEngineProvider injectFirst>
       <App />
     </StyledEngineProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
