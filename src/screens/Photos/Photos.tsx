80c809d119f4f4d0647b7ca40e1a07dddc79c589
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2025-07-12 15:34:44 +0530

    Integrate select bar to grid selection

--- src/screens/Photos/Photos.tsx
+++ src/screens/Photos/Photos.tsx
@@ -6,8 +6,14 @@ import type {
   SectionType,
 } from 'scrubbable-photo-grid/dist/types';
 
-import { createRef } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import images from '../../../data/images.json';
+import type { SelectBarControl } from 'src/components/SelectBar/types';
+
+import DeleteIcon from '@mui/icons-material/Delete';
+import ShareIcon from '@mui/icons-material/Share';
+import SelectBar from 'src/components/SelectBar/SelectBar';
+import type { GridSelectionHandler } from 'scrubbable-photo-grid/dist/Grid/types';
 
 const gridData = images.slice(0, 10) as SectionType[];
 
@@ -19,12 +25,52 @@ const gridConfig: GridConfigType = {
 };
 
 const Photos = () => {
-  const ref = createRef<HTMLDivElement>();
+  const selectBarControls: SelectBarControl[] = [
+    {
+      icon: DeleteIcon,
+      handler: () => alert('deleted'),
+    },
+    {
+      icon: ShareIcon,
+      handler: () => alert('shared'),
+    },
+  ];
+
+  const [open, setOpen] = useState<boolean>(false);
+  const [selections, setSelections] = useState<number>(0);
+
+  const selectHandler = useCallback<GridSelectionHandler>((selections) => {
+    if (Object.keys(selections).length === 0) {
+      return setOpen(false);
+    }
+
+    setSelections(
+      Object.values(selections).flatMap((segments) =>
+        Object.values(segments).flatMap((seg) => seg)
+      ).length
+    );
+    setOpen(true);
+  }, []);
+
+  const ref = useRef<HTMLDivElement>(null);
 
   return (
-    <div className='photos' ref={ref}>
-      <Grid gridData={gridData} config={gridConfig} parent={ref} />
-    </div>
+    <>
+      <SelectBar
+        open={open}
+        selections={selections}
+        onClose={() => setOpen(false)}
+        controls={selectBarControls}
+      />
+      <div className='photos' ref={ref}>
+        <Grid
+          gridData={gridData}
+          config={gridConfig}
+          parent={ref}
+          onSelect={selectHandler}
+        />
+      </div>
+    </>
   );
 };
 
